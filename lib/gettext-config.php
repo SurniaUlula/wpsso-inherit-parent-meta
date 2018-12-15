9c1ea8681e9cbd6cf60a7ab6c43992c43e4213ec
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:41:53 -0600

    v1.2.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,3 +1,6 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
 _x( 'WPSSO Core add-on to inherit the featured image and custom images from parents for posts, pages, custom post types, categories, tags, and custom taxonomies.', 'plugin description', 'wpsso-inherit-parent-meta' );
