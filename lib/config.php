5ba779c1b988f302509d15f54801a32bf946283c
JS Morisset <jsm@surniaulula.com>
2019-06-17 08:54:07 -0700

    v2.0.0-dev.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoipm' => array(			// Plugin acronym.
-					'version'     => '1.2.2',	// Plugin version.
+					'version'     => '2.0.0-dev.1',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO IPM',	// Short plugin name.
 					'name'        => 'WPSSO Inherit Parent Meta',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 					'req' => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '4.29.0',
+						'min_version' => '5.0.0-dev.1',
 					),
 					'assets' => array(
 						'icons' => array(
@@ -38,9 +38,9 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 						),
 					),
 					'lib' => array(
-						'gpl' => array(
-						),
 						'pro' => array(
+						),
+						'std' => array(
 						),
 					),
 				),
