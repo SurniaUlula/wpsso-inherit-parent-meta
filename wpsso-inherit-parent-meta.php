5c88cd5e78feb3ca2e9107358193b59bb94be51c
JS Morisset <jsm@surniaulula.com>
2019-06-18 13:03:47 -0700

    v2.0.0-dev.2

--- wpsso-inherit-parent-meta.php
+++ wpsso-inherit-parent-meta.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on to inherit featured and custom images from parents for posts, pages, custom post types, categories, tags, and custom taxonomies.
  * Requires At Least: 3.8
  * Tested Up To: 5.2.1
- * Version: 2.0.0-dev.1
+ * Version: 2.0.0-dev.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
