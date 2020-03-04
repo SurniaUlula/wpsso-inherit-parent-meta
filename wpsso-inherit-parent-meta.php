6a36e964eca09e2dbab1848d84f7efd7d138f20e
JS Morisset <jsm@surniaulula.com>
2020-03-04 14:12:30 -0800

    v2.1.0

--- wpsso-inherit-parent-meta.php
+++ wpsso-inherit-parent-meta.php
@@ -11,7 +11,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Inherit featured and custom images from parents for posts, pages, custom post types, categories, tags, and custom taxonomies.
- * Requires PHP: 5.5
+ * Requires PHP: 5.6
  * Requires At Least: 4.0
  * Tested Up To: 5.4
  * Version: 2.1.0
