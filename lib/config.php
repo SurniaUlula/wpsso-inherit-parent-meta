c0d9ae23924f33c9717b69fb79ddfd5c9dc51066
JS Morisset <jsm@surniaulula.com>
2020-08-11 12:49:28 -0700

    v3.3.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoipm' => array(			// Plugin acronym.
-					'version'     => '3.3.0-b.3',	// Plugin version.
+					'version'     => '3.3.0-rc.1',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO IPM',	// Short plugin name.
 					'name'        => 'WPSSO Inherit Parent Metadata',
@@ -43,7 +43,7 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '8.0.0-b.3',
+							'min_version'   => '8.0.0-rc.1',
 						),
 					),
 
@@ -85,7 +85,7 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 					'p_img_url'    => null,
 
 					/**
-					 * Twitter Card.
+					 * Twitter Cards.
 					 */
 					'tc_lrg_img_id'     => null,
 					'tc_lrg_img_id_pre' => null,
@@ -101,6 +101,7 @@ if ( ! class_exists( 'WpssoIpmConfig' ) ) {
 					'schema_img_max'    => null,
 					'schema_img_id'     => null,
 					'schema_img_id_pre' => null,
+					'schema_img_url'    => null,
 				),
 			),
 		);
