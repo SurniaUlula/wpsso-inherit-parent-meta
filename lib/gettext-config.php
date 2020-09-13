d1cbfa877995f2634a32c9b1ca9d57b57d967ac3
JS Morisset <jsm@surniaulula.com>
2020-09-13 10:51:18 -0700

    v3.3.0

--- lib/gettext-config.php
+++
@@ -1,6 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable text strings from the static config array.
- */
-_x( 'Inherit the Featured and Custom Images from Parents for Posts, Pages, Custom Post Types, Categories, Tags, and Custom Taxonomies.', 'plugin description', 'wpsso-inherit-parent-meta' );
