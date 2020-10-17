9fc2b3f67cfe13c4e2eabbc410d88e20bc16c86f
JS Morisset <jsm@surniaulula.com>
2020-10-17 10:05:54 -0700

    v3.4.0

--- wpsso-inherit-parent-meta.php
+++ wpsso-inherit-parent-meta.php
@@ -66,7 +66,7 @@ if ( ! class_exists( 'WpssoIpm' ) ) {
 			load_plugin_textdomain( 'wpsso-inherit-parent-meta', false, 'wpsso-inherit-parent-meta/languages/' );
 		}
 
-		public function init_objects( $is_admin, $doing_ajax, $doing_cron ) {
+		public function init_objects( $is_admin ) {
 
 			$this->p =& Wpsso::get_instance();
 
