0110822e515f115d8ed5b1f95347a581faeae096
JS Morisset <jsm@surniaulula.com>
2021-03-08 09:13:31 -0800

    v3.6.0-rc.1

--- wpsso-inherit-parent-meta.php
+++ wpsso-inherit-parent-meta.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.0
  * Requires At Least: 4.7
  * Tested Up To: 5.7
- * Version: 3.6.0-b.1
+ * Version: 3.6.0-rc.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
